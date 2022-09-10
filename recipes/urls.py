dd27ae63e098036fc15ee44fc67c346d4aea6d3c
suyash2002 <suyashbarwal@gmail.com>
2022-09-10 12:37:01 +0530

    add search functionality

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import RecipeDetailView, RecipeListView,RecipeCreateView,RecipeUpdateView,RecipeDeleteView,SearchView
+from .views import RecipeDetailView, RecipeListView,RecipeCreateView,RecipeUpdateView,RecipeDeleteView,search
 urlpatterns = [  
     path('new/',RecipeCreateView.as_view(),name="recipe_new"),
     path('', RecipeListView.as_view(), name="recipes_home"),
     path('<int:pk>/', RecipeDetailView.as_view(), name="recipe_detail"),
     path('<int:pk>/update/',RecipeUpdateView.as_view(),name="recipe_edit"),
     path('<int:pk>/delete/',RecipeDeleteView.as_view(),name="recipe_delete"),
-    path('search/',SearchView,name="search"),
+    path('search/',search,name="search"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+
+from django.shortcuts import render,HttpResponse
 from django.views.generic import ListView,DetailView
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from recipes.models import Recipe
 from django.urls import reverse_lazy
+from django.db.models import Q
 class RecipeListView(ListView):
   model=Recipe
   template_name='index.html'
@@ -26,7 +28,8 @@ class RecipeDeleteView(DeleteView):
       template_name='recipe_delete.html'
       success_url=reverse_lazy('home')
 
-def SearchView(request):
-      return render(request,
-                    'search.html',
-                    {})
+def search(request):
+      query=request.GET['query']
+      allRecipes=Recipe.objects.filter(Q(title__icontains=query) | Q(ingredients__icontains=query) )
+      params={'allRecipes':allRecipes}
+      return render(request,'search.html',params)
